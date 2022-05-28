930e57ba2d053716279ea8fd1bc25af337b9a290
JeongSeon-Lee <ljs658794@gmail.com>
2022-05-28 22:06:10 +0900

    혼공 예제 11-4, 버퍼를 사용하는 문자 입력

---
+++ honGong11/honGongE11-4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    char ch;
+    int i;
+
+    for (i = 0; i < 3; i++) {   // 첫 번째 : 입력 받고, 첫 번째 문자 저장, 출력
+        scanf("%c", &ch);       // 두 번째 : 입력 필요없고(버퍼에 남음), 두 번재 문자 저장, 출력
+        printf("%c", ch);       // 세 번재 : 입력 필요없고, 세 번째 문자 저장, 출력
+    }
+
+    return 0;
+}
