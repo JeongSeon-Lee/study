06ff21a24b2843db24a77380faeeec207e26b2a2
JeongSeon-Lee <ljs658794@gmail.com>
2022-05-28 22:28:20 +0900

    혼공 예제 11-5, stdio.h 헤더 파일에 EOF라는 이름을 -1로 바꾸는 전처리 지시자

--- honGong11/honGongE11-5.c
+++ honGong11/honGongE11-5.c
@@ -7,6 +7,7 @@ int main(void) {
     while (1) {
         res = scanf("%c", &ch);     // scanf 함수는 기본적으로 입력한 값의 개수를 반환
         if (res == -1) break;       // Ctrl+Z를 누르면 -1을 반환
+        // if (res == EOF) break;
         printf("%d ", ch);
     }
 
