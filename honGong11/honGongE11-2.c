312f992e79c4b911a6657e0e20e6f60bc7b52d84
JeongSeon-Lee <ljs658794@gmail.com>
2022-05-28 20:20:01 +0900

    혼공 예제 11-2, %c와 화이트 스페이스

--- honGong11/honGongE11-2.c
+++ honGong11/honGongE11-2.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 
 int main(void) {
+    int a, b;
     char ch1, ch2;
 
+    /* %c는 제어 문자, 화이트 스페이스도 입력됨 */
+    printf("문자 두 개 입력 : \n");
     scanf("%c%c", &ch1, &ch2);
+    printf("[%c%c]\n", ch1, ch2);     // 제어 문자를 %c로 출력하면 제어 문자의 기능을 수행
+    printf("%d, %d\n", ch1, ch2);     // 눈에 잘 보이게 아스키 코드 값으로 출력
 
-    printf("[%c%c]", ch1, ch2);
+    /* %d, %lf, %s에서 화이트 스페이스는 데이터 구분 용도 */
+    printf("정수 두 개 입력 : \n");
+    scanf("%d%d", &a, &b);
+    printf("%d, %d\n", a, b);
+
+    printf("정수 두 개 입력 : \n");
+    scanf("%d %d", &a, &b);
+    printf("%d, %d\n", a, b);
+
+    printf("문자 두 개 입력 : \n");
+    scanf(" %c %c", &ch1, &ch2);
+    printf("[%c%c]\n", ch1, ch2);
 
     return 0;
 }
