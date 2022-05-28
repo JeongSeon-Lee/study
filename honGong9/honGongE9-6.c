5df9237f8ced8ce47f89aadae69711e257c14235
JeongSeon-Lee <ljs658794@gmail.com>
2022-05-28 14:43:10 +0900

    혼공 예제 9-6, 혀용되지 않는 포인터의 대입

--- honGong9/honGongE9-6.c
+++ honGong9/honGongE9-6.c
@@ -5,7 +5,7 @@ int main(void) {
     int *p = &a;
     double *pd;
 
-    pd = p;
+    pd = p;     // p 주소값을 pd에 대입, 주소를 100이라 가정하면 *p는 100~103번지 영역, *pd는 100~107번지 영역
     printf("%lf\n", *pd);
 
     return 0;
