665a82fbc1251a441131a9d87215afa886f26500
JeongSeon-Lee <ljs658794@gmail.com>
2022-05-28 14:57:48 +0900

    혼공 예제 9-6 +, 대입해보기

--- honGong9/honGongE9-6+.c
+++ honGong9/honGongE9-6+.c
@@ -6,8 +6,8 @@ int main(void) {
     int *pi;
 
     /* 형 변환을 사용한 포인터의 대입은 언제나 가능합니다. */
-/*     pi = (int *)pd;
-    printf("%d\n", *pi); */
+    pi = (int *)pd;
+    printf("%d\n", *pi);
 
     /* 포인터에 100번지 직접 대입 */
     pi = (int *) 100;
