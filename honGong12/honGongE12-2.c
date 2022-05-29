6eec624523c3a95fb16d5b1731045e81d9430713
JeongSeon-Lee <ljs658794@gmail.com>
2022-05-29 12:10:12 +0900

    혼공 예제 12-2, 포인터로 문자열을 사용하는 방법

---
+++ honGong12/honGongE12-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    char *dessert = "apple";    // 포인터에 문자열 초기화
+
+    printf("오늘 후식은 %s입니다.\n", dessert);
+    dessert = "banana";
+    printf("내일 후식은 %s입니다.\n", dessert);
+
+    return 0;
+}
