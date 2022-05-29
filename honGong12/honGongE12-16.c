66449f9f3075206953221f47f1d2f04b546b98f4
JeongSeon-Lee <ljs658794@gmail.com>
2022-05-29 15:58:27 +0900

    혼공 예제 12-16, 직접 만드는 strcmp 함수 책 버전

--- honGong12/honGongE12-16.c
+++ honGong12/honGongE12-16.c
@@ -17,6 +17,15 @@ int main(void) {
 }
 
 int my_strcmp(char *pd, char *ps) {
+    while ((*pd == *ps) && (*pd != '\0')) {     // 두 문자가 같으나 널 문자가 아닌 경우
+        pd++;                                   // 다음 문자로 이동
+        ps++;
+    }
+    /* 반복문 이후 이 시점에서는 두 문자가 다르거나 둘 다 널 문자임 */
+    if (*pd > *ps) return 1;                    // 앞 문자의 아스키 코드 값이 크면 1 반환
+    else if (*pd < *ps) return -1;              // 뒷 문자의 아스키 코드 값이 크면 -1 반환
+    else return 0;                              // 둘 다 널 문자이므로 같은 문자열
+/*     
     if (*pd == *ps) {
         if (*pd == '\0')
             return 0;
@@ -27,4 +36,5 @@ int my_strcmp(char *pd, char *ps) {
         return 1;
     else
         return -1;
+ */
 }
