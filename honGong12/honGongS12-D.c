a1fc18159e4949199dffe32c02a069fd2f7d1b5c
JeongSeon-Lee <ljs658794@gmail.com>
2022-05-29 16:58:40 +0900

    혼공 두번째 마무리 12-D, 단어 정렬 프로그램 해답 버전

--- honGong12/honGongS12-D.c
+++ honGong12/honGongS12-D.c
@@ -4,7 +4,31 @@
 int main(void) {
     char str1[80], str2[80], str3[80];
     char high[80], middle[80], low[80], temp[80];
+    
+    // st1이 str2보다 사전의 뒤에 나오면 두 문자열을 바꾼다
+     if(strcmp(str1, str2) > 0) 
+    {
+        strcpy(temp, str1);
+        strcpy(str1, str2);
+        strcpy(str2, temp);
+    }
 
+    // st1이 str3보다 사전의 뒤에 나오면 두 문자열을 바꾼다
+    if(strcmp(str1, str3) > 0) 
+    {
+        strcpy(temp, str1);
+        strcpy(str1, str3);
+        strcpy(str3, temp);
+    }
+
+    // st2이 str3보다 사전의 뒤에 나오면 두 문자열을 바꾼다
+    if(strcmp(str2, str3) > 0) 
+    {
+        strcpy(temp, str2);
+        strcpy(str2, str3);
+        strcpy(str3, temp);
+    }
+/* 
     printf("세 단어 입력 : ");
     scanf("%s %s %s", str1, str2, str3);
     
@@ -28,7 +52,7 @@ int main(void) {
         strcpy(middle, low);
         strcpy(low, str3);
     }
-
+ */
     printf("%s, %s, %s", low, middle, high);
 
     return 0;
