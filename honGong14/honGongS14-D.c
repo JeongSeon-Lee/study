88acd3afa7ec989ed32ffe6f2ae79275fc8d45cc
JeongSeon-Lee <ljs658794@gmail.com>
2022-05-30 21:37:02 +0900

    혼공 두번째 마무리 14-D, 가로 세로의 합 구하기

---
+++ honGong14/honGongS14-D.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void) {
+    int ary[5][6] = {
+        { 1, 2, 3, 4, 5 }, 
+        { 6, 7, 8, 9, 10 }, 
+        { 11, 12, 13, 14, 15 }, 
+        { 16, 17, 18, 19, 20 }
+    };
+    int i, j, sum = 0;
+
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 5; j++) {
+            sum += ary[i][j];
+        }
+        j++;
+        ary[i][j] = sum;
+    }
+    
+    sum = 0;
+    for (i = 0; i < 6; i++) {
+        for (j = 0; j < 4; j++) {
+            sum += ary[j][i];
+        }
+        j++;
+        ary[j][i] = sum;
+    }
+
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 6; j++) {
+            printf("%5d", ary[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
