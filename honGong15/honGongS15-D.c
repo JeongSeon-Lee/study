541d991497d0390750ff62f92f878261b6d476c5
JeongSeon-Lee <ljs658794@gmail.com>
2022-06-02 15:20:10 +0900

    혼공 두번째 마무리 15-D, 프로필 교환 프로그램

---
+++ honGong15/honGongS15-D.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void swap(char *type, void *a, void *b);
+
+int main(void) {
+    int w1, w2;
+    double h1, h2;
+
+    printf("첫 번째 사람의 나이와 키 입력 : ");
+    scanf("%d %lf", &w1, &h1);
+    printf("두 번째 사람의 나이와 키 입력 : ");
+    scanf("%d %lf", &w2, &h2);
+
+    swap("int", &w1, &w2);
+    swap("double", &h1, &h2);
+
+    printf("첫 번째 사람의 나이와 키 : %d, %.1lf", w1, h1);
+    printf("두 번째 사람의 나이와 키 : %d, %.1lf", w2, h2);
+
+    return 0;
+}
+
+void swap(char *type, void *a, void *b) {
+    void *temp;
+
+    if (type == "int") {
+        (int *)temp = (int *)a;
+        (int *)a = (int *)b;
+        (int *)b = (int *)temp;
+    }
+    else if (type == "double") {
+        (double *)temp = (double *)a;
+        (double *)a = (double *)b;
+        (double *)b = (double *)temp;
+    }
+}
