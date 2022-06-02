6f29b62d9d6aeb742cfe4d40467478206c11c529
JeongSeon-Lee <ljs658794@gmail.com>
2022-06-02 15:47:26 +0900

    void *tmep 포인터를 초기화하지 않고 선언했는데, *(int *)temp으로 가리키는 값(없는데) 사용해서 그런 듯

--- honGong15/honGongS15-D.c
+++ honGong15/honGongS15-D.c
@@ -15,23 +15,40 @@ int main(void) {
     swap("int", &w1, &w2);
     swap("double", &h1, &h2);
 
-    printf("첫 번째 사람의 나이와 키 : %d, %.1lf", w1, h1);
-    printf("두 번째 사람의 나이와 키 : %d, %.1lf", w2, h2);
+    printf("첫 번째 사람의 나이와 키 : %d, %.1lf\n", w1, h1);
+    printf("두 번째 사람의 나이와 키 : %d, %.1lf\n", w2, h2);
 
     return 0;
 }
 
-void swap(char *type, void *a, void *b) {
+/* void swap(char *type, void *a, void *b) {
     void *temp;
 
-    if (strcmp(type, "int") == 0) {
+    if(type == "int") {
         *(int *)temp = *(int *)a;
         *(int *)a = *(int *)b;
         *(int *)b = *(int *)temp;
     }
-    else if (strcmp(type, "double") == 0) {
+    if(type == "double") {
         *(double *)temp = *(double *)a;
         *(double *)a = *(double *)b;
         *(double *)b = *(double *)temp;
     }
+}
+ */
+void swap(char *type, void *a, void *b) {
+    int temp;
+    double dtemp;
+
+    if(type == "int") {
+        temp = *(int *)a;
+        *(int *)a = *(int *)b;
+        *(int *)b = temp;
+     }
+     if(type == "double") {
+        dtemp = *(double *)a;
+        *(double *)a = *(double *)b;
+        *(double *)b = dtemp;
+     }
+
 }
